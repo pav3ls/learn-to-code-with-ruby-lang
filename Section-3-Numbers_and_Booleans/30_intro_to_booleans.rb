395ac5e3319635e79cf8c78a4727f405f5e1bb53
Pavels <pavels.zaborovskis@gmail.com>
2021-07-02 23:40:18 +0100

    Add lesons 29...34

---
+++ Section-3-Numbers_and_Booleans/30_intro_to_booleans.rb
@@ -0,0 +1,7 @@
+p 5 < 10
+p 10 > 12
+
+age = 25
+handsome = true
+
+p handsome.class
---
+++ Section-3-Numbers_and_Booleans/33_comparison_with_the_inequality_operator.rb
@@ -0,0 +1,9 @@
+p 10 != 5 
+p 10 != 10
+
+puts 'Strings'
+p "Hello " != "Goodbye"
+p "Hello" != "hello"
+p "Hello".downcase != "hello".downcase
+
+p "123" != 123
