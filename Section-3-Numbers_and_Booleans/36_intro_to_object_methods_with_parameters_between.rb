bbf5d82490c3b14391cfc55f8f53420ff98c0313
Pavels <pavels.zaborovskis@gmail.com>
2021-07-26 22:51:00 +0100

    Add lesons 37 to 38 on Section 3

---
+++ Section-3-Numbers_and_Booleans/36_intro_to_object_methods_with_parameters_between.rb
@@ -0,0 +1,4 @@
+p 20.between?(10, 30)
+p 20.between?(30, 50)
+
+p 1.2.between?(1.1, 1.3)
---
+++ Section-3-Numbers_and_Booleans/37_float_methods.rb
@@ -0,0 +1,11 @@
+p 10.4.to_i
+p 10.4.to_i.class 
+
+p 10.5.floor
+p 10.5.ceil
+
+p 3.6.round
+p 3.14159.round(3)
+
+p 35.67.abs 
+p -35.67.abs 
