bc156de328c389f2202de1de96296cb691aa36f2
Pavels <pavels.zaborovskis@gmail.com>
2021-07-29 22:46:40 +0100

    Add lesson 42

---
+++ Section-4-Strings-I/42_create_a_string.rb
@@ -0,0 +1,23 @@
+puts "Hi there, I'm a string"
+
+puts "I can also include charecyet and numbers: # $ ! @ 5 9"
+
+name = "Pavel"
+surename = "Zab" 
+
+puts name, surename
+
+space = " "
+p space.length
+
+puts
+
+empty = ""
+p empty.length
+
+p name.class
+
+name2 = String.new("Pavel")
+puts name2
+
+p 5.to_s.class
